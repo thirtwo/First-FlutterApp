25be0d82ad632c55bafd8128822d4a34e7e06bf3
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-08-27 02:01:34 +0300

    App bar and background color changed

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,16 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget 
-{ 
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) 
-  {
-    
+  Widget build(BuildContext context) {
     var appName = WordPair.random();
     return MaterialApp(
       title: appName.asSnakeCase,
       home: Scaffold(
+        backgroundColor: Colors.blue[100],
         appBar: AppBar(
+          backgroundColor: Colors.blue[300],
           title: Text(appName.toString()),
         ),
         body: Center(
@@ -33,6 +32,6 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     final randomText = WordPair.random();
-      return Text(randomText.asPascalCase);  
+    return Text(randomText.asPascalCase);
   }
 }
